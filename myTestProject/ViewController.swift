389c7b45b7e147bd6862550af43af121aa1f532d
MeghanaKallepalli <meghanak@ahaicoe.org>
2025-01-24 17:06:05 +0530

    Initial Commit

---
+++ myTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTestProject
+//
+//  Created by SS-MAC-004 on 08/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ myTestProjectTests/myTestProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  myTestProjectTests.swift
+//  myTestProjectTests
+//
+//  Created by SS-MAC-004 on 08/01/25.
+//
+
+import Testing
+@testable import myTestProject
+
+struct myTestProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
